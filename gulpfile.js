c949b50d05c0a3f7b1e0f8bf989274e38d3ed21c
peytonchance <peytonchance@gmail.com>
2018-02-06 11:59:43 -0500

    Updated homepage styles.

--- gulpfile.js
+++ gulpfile.js
@@ -21,7 +21,7 @@ gulp.task('connect', function(){
   app.set('view engine', 'ejs');
 
   app.listen(app.get('port'), function() {
-    console.log('Node app WOO is running on port', app.get('port'));
+    console.log('Node app is running on port', app.get('port'));
   });
 
   app.get('/', function(request, response) {
